b8e5cea6aaedbc4f81eb5766cd59ccfd4bea1bb6
Peter (Cytronicode) <92656878+Cytronicode@users.noreply.github.com>
2022-03-17 06:37:43 +0800

    Updated packages; experimental changes with buttons

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
 
 // Light and dark mode
 import { useColorMode } from '@chakra-ui/color-mode'
@@ -10,50 +8,53 @@ import {
   MoonIcon,
   SunIcon
 } from '@chakra-ui/icons';
-import { IconButton } from '@chakra-ui/button'
 
-import styles from '../styles/Home.module.css' 
+import { Button, IconButton } from '@chakra-ui/button'
 
-export default function Home() { 
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
 
 	const { colorMode, toggleColorMode } = useColorMode()
-	
-	return ( 
-		<div className={styles.container}> 
-			<Head> 
+
+	return (
+		<div className={styles.container}>
+			<Head>
 				<title>Cogniforge</title>
 				<meta name="description" content="Learning for all through collaborative projects and peer-to-peer mentorship" />
 				<meta httpEquiv="content-type" content="text/html; charset=utf-8" />
 				<meta httpEquiv="content-language" content="en_US" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-		
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" /> 
+
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-				<link rel="manifest" href="/site.webmanifest" /> 
+				<link rel="manifest" href="/site.webmanifest" />
 			</Head>
-			
-			<main className={styles.main}>
-				<img src="/cogniforge.svg" alt="Cogniforge logo" className={styles.logo}/> 
-			
-				<h1 className={styles.title}> Cogniforge Alpha </h1>
-			
-				<div className={styles.descriptionDiv}>
-					<p className={styles.description}> You have been invited to the alpha testing of Cogniforge, <span className={styles.underlined}>a free project-based learning community.</span> </p>
-		
-					<p className={styles.note}> Note: Alpha releases will be introducing new features continuously, so please keep checking back for updates! </p>
-				</div>
-			
-				<div className={styles.buttonsDiv}>
-					<button className={`${styles.button} ${styles.login}`}> Log in </button>
-					<button className={`${styles.button} ${styles.signup}`}> Sign up </button> 
-				</div> 
 
-				<IconButton mt={4} size='sm' aria-label="Toggle light/dark mode" onClick={toggleColorMode}>
+			<main className={styles.main}>
+				<img src="/cogniforge.svg" alt="Cogniforge logo" className={styles.logo}/>
+
+				<div className={styles.hero}>
+					<h1 className={styles.title}> Cogniforge Alpha </h1>
+
+					<p className={styles.description}> You have been invited to the alpha testing of Cogniforge, <span className={styles.underlined}>a free project-based learning community.</span> </p>
+				</div>
+
+        <Stack spacing={4} direction='row' align='center'>
+          <Button colorScheme='purple'>
+            Login
+          </Button>
+          <Button colorScheme='black'>
+            Sign up
+          </Button>
+        </Stack>
+
+				<IconButton aria-label="Toggle light/dark mode" onClick={toggleColorMode}>
           {colorMode === 'light' ? <MoonIcon/> : <SunIcon/>}
         </IconButton>
-				
-			</main> 
-		</div> 
-	) 
+
+			</main>
+		</div>
+	)
 }
